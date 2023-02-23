a1d62059abcbe19ddd13a5e49ef48d6b7270f4fc
Maleila <maleila.henrymitchell@gmail.com>
2023-02-23 16:01:44 -0600

    set up Michelle's code in a vue template

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Exquisite from './components/Exquisite.vue';
+import HelloWorld from './components/Exquisite.vue'
+</script>
+
+<template>
+  <Exquisite msg="Exquisite" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Exquisite.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    data() {
+      return {
+        message: '',
+        count : 0,
+        players : 1,
+        ishidden : true, //what specifically does this describe? like what's hidden or not?
+        finished: false,
+        story: '',
+        msg1: ''
+      }
+    }
+}
+</script>
+
+<template>
+  <div class = "start">
+    <h1>
+      Exquisite Corpse
+    </h1>
+    <h2 v-if= "count == 0">
+    Number of players: {{players}}
+    </h2>
+    <input v-if= "count == 0" v-model="players">
+      <br>
+      <br>
+    <button v-on:click="count++ , ishidden = false" v-if="count == 0">Submit</button>
+    
+    <h2 v-for= "i in players" v-if="!ishidden && count <= players">
+    Player {{count}}/{{players}} write your sentence:
+      <br>
+      {{msg1}}
+      <br>
+      {{message}}
+      <br>
+    <input v-model="message">
+    </h2>
+    
+    <button v-if="!ishidden && count <= players" v-on:click="count++ , story = story.concat(message + ' '), msg1 = message, message = ''">Submit</button>
+      
+
+    <button v-on:click= "finished = true, ishidden = ! ishidden" v-if="count > players && !ishidden">
+      View Story
+    </button>
+    
+    <h3 v-if="finished && count > players">{{story}}</h3>
+    
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
