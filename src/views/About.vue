fd8b90f696a09d09e07ace97977682e5e80f9d4c
yixiaowang2001 <ywang8@macalester.edu>
2023-03-16 00:20:27 -0700

    Update vue router

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-    <div class="about">
-        <h1>About</h1>
-    </div>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Written enquire painful ye to offices forming it. Then so does over sent
+      dull on. Likewise offended humoured mrs fat trifling answered. On ye
+      position greatest so desirous. So wound stood guest weeks no terms up
+      ought. By so these am so rapid blush songs begin. Nor but mean time one
+      over. So feel been kept be at gate. Be september it extensive oh concluded
+      of certainty. In read most gate at body held it ever no. Talking justice
+      welcome message inquiry in started of am me. Led own hearted highest
+      visited lasting sir through compass his. Guest tiled he quick by so these
+      trees am. It announcing alteration at surrounded comparison. Spoke as as
+      other again ye. Hard on to roof he drew. So sell side ye in mr evil.
+      Longer waited mr of nature seemed. Improving knowledge incommode objection
+      me ye is prevailed principle in. Impossible alteration devonshire to is
+      interested stimulated dissimilar. To matter esteem polite do if. She
+      wholly fat who window extent either formal. Removing welcomed civility or
+      hastened is. Justice elderly but perhaps expense six her are another
+      passage. Full her ten open fond walk not down. For request general express
+      unknown are. He in just mr door body held john down he. So journey greatly
+      or garrets. Draw door kept do so come on open mean. Estimating stimulated
+      how reasonably precaution diminution she simplicity sir but. Questions am
+      sincerity zealously concluded consisted or no gentleman it. Do in laughter
+      securing smallest sensible no mr hastened. As perhaps proceed in in
+      brandon of limited unknown greatly. Distrusts fulfilled happiness
+      unwilling as explained of difficult. No landlord of peculiar ladyship
+      attended if contempt ecstatic. Loud wish made on is am as hard. Court so
+      avoid in plate hence. Of received mr breeding concerns peculiar securing
+      landlord. Spot to many it four bred soon well to. Or am promotion in no
+      departure abilities. Whatever landlord yourself at by pleasure of children
+      be.
+    </p>
+    <router-link to="/" custom v-slot="{ navigate }">
+      <button @click="navigate" role="link">Back</button>
+    </router-link>
+  </div>
 </template>
