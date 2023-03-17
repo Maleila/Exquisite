bd41f78fd89b926adc586e0306787a2aa82165b1
yixiaowang2001 <ywang8@macalester.edu>
2023-03-17 01:47:20 -0700

    Update style

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "../public/style.css";
+// import "../public/style.css";
 
-// // Uncomment these two lines
-// import "../public/main.css";
-// import "../public/css/home.css";
+// Comment these two lines
+import "../public/main.css";
+import "../public/css/home.css";
 
 createApp(App).use(router).mount("#app");
