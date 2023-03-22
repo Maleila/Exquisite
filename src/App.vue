824072c5e3153209cdd65fe7334681e8a861de90
mdong10 <mdong@macalester.edu>
2023-03-22 15:57:11 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup>
-import Exquisite from "./views/Exquisite.vue";
-import HelloWorld from "./views/Exquisite.vue";
+import Exquisite from './views/Exquisite.vue'
+import {ref , onMounted} from 'vue'
+import db from '@/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+
+
+// import HelloWorld from "./views/Exquisite.vue";
+onMounted(async() => {
+  const querySnapshot = await getDocs(collection(db, "exquisite"));
+  let gameValues = []
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data())
+    const play = {
+      id : doc.id,
+      game: doc.data().game,
+      player: doc.data().player,
+      message: doc.data().message
+    }
+    gameValues.push(play)
+  })
+  gameValuesM.value = gameValues
+})
+
 </script>
 
 <template>
   <!-- <Exquisite /> -->
   <br />
-  <div class="cotainer">
+  <div class="container">
     <router-view />
-    <!-- here we want the home compoenet to show up  -->
+    <!-- here we want the home component to show up  -->
   </div>
 </template>
+
+<style scoped>
+@import 'bulma/css/bulma.min.css';
+</style>
