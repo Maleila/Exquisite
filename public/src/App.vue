c986927fe92498ee7f4236138acfe969e1a9218a
Dennis Qin <89886880+Darjeel1ng@users.noreply.github.com>
2023-03-30 02:54:21 +0800

    put the main game in public

---
+++ public/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Exquisite from "./views/Exquisite.vue";
+import HelloWorld from "./views/Exquisite.vue";
+</script>
+
+<template>
+  <br />
+  <div class="cotainer">
+    <router-view />
+  </div>
+</template>
